eb35cb76febc7ddcee240d6f0c8a9778675a5fb4
cbrauers <c.brauers@gmx.de>
2024-06-01 17:37:31 +0200

    Fixed encoder IDs and startup event spam Updated definition files

--- MobiFlight/Joysticks/VKB/VKBEncoder.cs
+++ MobiFlight/Joysticks/VKB/VKBEncoder.cs
@@ -10,28 +10,48 @@ namespace MobiFlight.Joysticks.VKB
             DEC = 0,
             INC = 5
         }
+        private bool firstStart = false;
         public JoystickDevice DeviceInc = null;
         public JoystickDevice DeviceDec = null;
         private ushort value = 0;
-        public VKBEncoder(byte index, ushort initialValue = 0)
+        public VKBEncoder(byte index, ushort? initialValue = null)
         {
-            value = initialValue;
-            CreateDevices(index + 1);
+            if (initialValue == null) firstStart = true;
+            else
+            {
+                value = initialValue?? 0;
+                firstStart = false;
+            }
+            CreateDevices(index);
         }
-        public VKBEncoder(JoystickDevice inc, JoystickDevice dec, ushort initialValue = 0)
+        public VKBEncoder(JoystickDevice inc, JoystickDevice dec, ushort? initialValue = null)
         {
             DeviceInc = inc;
             DeviceDec = dec;
-            value = initialValue;
+            if (initialValue == null)
+            {
+                firstStart = true;
+            }
+            else
+            {
+                value = initialValue ?? 0;
+                firstStart = false;
+            }
         }
         private void CreateDevices(int index)
         {
-            DeviceInc = new JoystickDevice { Name = $"Button {1000 + 10 * index + (int)EncoderAction.INC}", Label = $"Encoder {index} INC", Type = DeviceType.Button, JoystickDeviceType = JoystickDeviceType.Button };
-            DeviceDec = new JoystickDevice { Name = $"Button {1000 + 10 * index + (int)EncoderAction.DEC}", Label = $"Encoder {index} DEC", Type = DeviceType.Button, JoystickDeviceType = JoystickDeviceType.Button };
+            DeviceInc = new JoystickDevice { Name = $"Button {1000 + 10 * index + (int)EncoderAction.INC}", Label = $"Encoder {index+1} INC", Type = DeviceType.Button, JoystickDeviceType = JoystickDeviceType.Button };
+            DeviceDec = new JoystickDevice { Name = $"Button {1000 + 10 * index + (int)EncoderAction.DEC}", Label = $"Encoder {index+1} DEC", Type = DeviceType.Button, JoystickDeviceType = JoystickDeviceType.Button };
         }
         public IEnumerable<InputEventArgs> Update(ushort newPosition)
         {
             List<InputEventArgs> events = new List<InputEventArgs>();
+            if (firstStart)
+            {
+                value = newPosition;
+                firstStart= false;
+                return events;
+            }
             short deltaCount = (short)((newPosition - value) & 0xFFFF);
             if (deltaCount > 0)
             {
